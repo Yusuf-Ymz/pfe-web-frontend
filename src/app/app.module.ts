db02d15c29896eebd339e091082c409aa093ef79
tahir-bashir09 <73584198+tahir-bashir09@users.noreply.github.com>
2020-12-03 00:55:49 +0100

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,20 @@ import { AuthentificationComponent } from './components/authentification/authent
 import { DoctorComponent } from './components/doctor/doctor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthentificationComponent,
-    DoctorComponent
+    DoctorComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +31,12 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     ToastrModule.forRoot({ positionClass: 'toast-bottom-right'}),
     BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
