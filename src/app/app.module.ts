73ff0e1170576a14ac1be2f2ea0fb8d88218319d
tahir-bashir09 <73584198+tahir-bashir09@users.noreply.github.com>
2020-12-03 01:22:23 +0100

    update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { NavbarComponent } from './components/navbar/navbar.component'
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
