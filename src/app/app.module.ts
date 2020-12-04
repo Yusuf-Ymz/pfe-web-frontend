8ccccaf951626710384c0496aaf0e8b436befca5
tahir-bashir09 <73584198+tahir-bashir09@users.noreply.github.com>
2020-12-04 14:04:56 +0100

    Revert "Update app.module.ts"
    This reverts commit 27a332905e81b9bfaf1cbc347a69cf64ac52e3ca.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,12 @@ import { DoctorComponent } from './components/doctor/doctor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { MaterialModule } from './modules/material/material.module';
 
 @NgModule({
   declarations: [
@@ -28,7 +32,11 @@ import { MaterialModule } from './modules/material/material.module';
     ToastrModule.forRoot({ positionClass: 'toast-bottom-right'}),
     BrowserAnimationsModule,
     LayoutModule,
-    MaterialModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
