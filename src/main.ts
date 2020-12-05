2f40c4e97f499359f93e380cf09249399a19d6af
YYIPL <yyusuf.ipl@gmail.com>
2020-12-05 06:23:32 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+fetch('/assets/config/config.json')
+  .then((response) => response.json())
+  .then((response) => {
+    if (response.environment.production) {
+      enableProdMode()
+    }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err))
+  });
