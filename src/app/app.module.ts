abda7b64454b402191876b09b3f946e1fd891a1b
tahir-bashir09 <73584198+tahir-bashir09@users.noreply.github.com>
2020-12-06 04:57:57 +0100

    add: CUSTOM_ELEMENTS_SCHEMA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NgForm, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
@@ -31,6 +31,7 @@ import { EstablishmentComponent } from './components/establishment/establishment
     LocationDetailsComponent,
     EstablishmentComponent,
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
     BrowserModule,
     AppRoutingModule,
