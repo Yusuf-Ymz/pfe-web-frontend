6aa43c799f6bc85f34dc77186719a16cfd2d662a
oussama.elbouenani <oussama.elbouenani@student.vinci.be>
2020-12-06 15:41:16 +0100

    add - finished register form

--- src/app/components/authentification/authentification.component.ts
+++ src/app/components/authentification/authentification.component.ts
@@ -1,21 +1,17 @@
-import { Component} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {AuthentificationService} from '../../services/authentification.service'
-import {MatOptionSelectionChange} from '@angular/material/core';
-import {stringify} from 'querystring';
-
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthentificationService } from '../../services/authentification.service';
 @Component({
   selector: 'app-authentification',
   templateUrl: './authentification.component.html',
-  styleUrls: ['./authentification.component.css']
+  styleUrls: ['./authentification.component.css'],
 })
-export class AuthentificationComponent  {
+export class AuthentificationComponent {
+  accountTypes = ['doctor', 'establishment'];
+  hide = true;
+  current = 'establishment';
 
-  accountTypes = ['doctor','establishment']
-  hide = true
-  current = '';
-
-  constructor(public authenficationService: AuthentificationService) { }
+  constructor(public authenficationService: AuthentificationService) {}
 
   onLogin(form: NgForm) {
     if (form.invalid) {
@@ -25,18 +21,36 @@ export class AuthentificationComponent  {
     const username = form.value.username;
     const password = form.value.password;
 
-    this.authenficationService.login(username,password)
+    this.authenficationService.login(username, password);
   }
 
   onRegister(form: NgForm) {
     if (form.invalid) {
       return;
     }
+
+    console.log(form.value);
+
+    const username = form.value.username;
+    const password = form.value.password;
+    const firstname = form.value.firstName;
+    const lastname = form.value.lastName;
+    const name = form.value.name;
+
+    console.log(username, password);
+
+    this.authenficationService.register(
+      this.current,
+      username,
+      password,
+      firstname,
+      lastname,
+      name
+    );
   }
 
-  onType(type: string){
+  onType(type: string) {
     console.log(type);
     this.current = type;
   }
-
 }
--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -1,45 +1,112 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
 import { loginData } from '../models/login-data.model';
 import { Router } from '@angular/router';
-import { ToastrService} from 'ngx-toastr'
-import { environment} from '../../environments/environment';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from '../../environments/environment';
+import { doctorData } from '../models/doctor-data.model';
+import { establishmentData } from '../models/establishment-data.model';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthentificationService {
-
-  constructor(private http: HttpClient, private router: Router, private toastr: ToastrService) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   login(username: string, password: string) {
     const loginData: loginData = {
       username: username,
-      password: password
+      password: password,
+    };
+
+    this.http
+      .post<{ token: string; account: any }>(
+        environment.serverUrl + 'login',
+        loginData
+      )
+      .subscribe(
+        (response) => {
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('account', JSON.stringify(response.account));
+
+          if (response.account.establishment !== undefined) {
+            this.router.navigate(['/establishment']);
+          } else {
+            this.router.navigate(['/doctor']);
+          }
+
+          this.toastr.success('Bienvenue!');
+        },
+        (error: HttpErrorResponse) => {
+          if (error.status === 404 || error.status === 401) {
+            //reponse  mdp ou username incorrect
+            this.toastr.error("Mot de passe/nom d'utilisateur incorrect");
+          } else {
+            //connexion fail
+            this.toastr.error('Impossible de se connecter');
+          }
+        }
+      );
+  }
+
+  register(
+    accountType: string,
+    username: string,
+    password: string,
+    firstname: string,
+    lastname: string,
+    name: string
+  ) {
+    if (accountType === 'doctor') {
+      const doctor: doctorData = {
+        username,
+        password,
+        doctor: {
+          first_name: firstname,
+          last_name: lastname,
+        },
+      };
+      this.doPostRegister(doctor);
+    } else if (accountType === 'establishment') {
+      const establishment: establishmentData = {
+        username,
+        password,
+        establishment: {
+          name: name,
+        },
+      };
+      this.doPostRegister(establishment);
+    } else {
+      this.toastr.error('invalid account type !');
+      return;
     }
+  }
 
-    this.http.post<{ token: string, account: any }>(environment.serverUrl+'login', loginData)
-      .subscribe(response => {
-
-        localStorage.setItem("token", response.token);
-        localStorage.setItem("account", JSON.stringify(response.account));
-        
-        if(response.account.establishment !== undefined){
-          this.router.navigate(['/establishment'])
+  doPostRegister(data: any) {
+    console.log('Je vais inscrire ', data);
+    this.http
+      .post<{ token: string; account: any }>(
+        environment.serverUrl + 'register',
+        data
+      )
+      .subscribe(
+        (response) => {
+          if (response.account.username === data.username) {
+            this.toastr.success('Bienvenue!');
+          } else {
+            this.toastr.error('Un problème est survenu !');
+          }
+        },
+        (error: HttpErrorResponse) => {
+          this.toastr.error(error.message);
         }
-        else {
-          this.router.navigate(['/doctor'])
-        }
-
-        this.toastr.success("Bienvenue!")
-      }, (error: HttpErrorResponse) => {
-        if (error.status === 404 || error.status === 401) {
-          //reponse  mdp ou username incorrect
-          this.toastr.error("Mot de passe/nom d'utilisateur incorrect")
-        } else {
-          //connexion fail
-          this.toastr.error("Impossible de se connecter")
-        }
-      });
+      );
   }
 }
-
