2ba0f05d3b08104cb6702d791c1847b6ed0343ae
Walid <walid.naji@student.vinci.be>
2020-12-07 19:55:05 +0100

    btn show hide disconnect

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Injectable, OnDestroy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { AuthentificationService } from '../../services/authentification.service'
 import { BehaviorSubject, Subscription } from 'rxjs';
+import {Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -18,21 +19,23 @@ export class NavbarComponent implements OnInit, OnDestroy{
   isAuthenticated: boolean = false;
   private authenticationListenerSubscription!: Subscription;
 
-  constructor( private authService : AuthentificationService) {
+  constructor( private authService : AuthentificationService, private router: ActivatedRoute) {
+    router.params.subscribe(val => {
+      this.isAuthenticated = this.authService.getIsAuthenticated();
+      this.authenticationListenerSubscription = this.authService.getAuthenticationStatusListener().subscribe(isAuthenticated => {
+        this.isAuthenticated = isAuthenticated;
     
+        if(this.isAuthenticated) this.afficherBoutonLg = true;
+        if(!this.isAuthenticated) this.afficherBoutonLg = false;
+      
+      })
+    })
   }
 
   ngOnInit(): void {
-    this.isAuthenticated = this.authService.getIsAuthenticated();
-    console.log("il est log ? ", this.isAuthenticated);
-
-    this.authenticationListenerSubscription = this.authService.getAuthenticationStatusListener().subscribe(isAuthenticated => {
-      this.isAuthenticated = isAuthenticated;
     
-      if(this.isAuthenticated) this.afficherBoutonLg = true;
-      if(!this.isAuthenticated) this.afficherBoutonLg = false;
-      
-    })
+
+    
     
     
   }
